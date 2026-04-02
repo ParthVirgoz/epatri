c2d6f5dbaf7b97239fe99ed38a4635ebf31ac8b9
parthvirgoz <imparthdudhatra@gmail.com>
2026-04-02 17:36:55 +0530

    VERCEL:fix-2

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,18 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
   },
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
 });
